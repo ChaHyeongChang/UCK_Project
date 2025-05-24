32967cb1e14e3f4538cca24795ed6853b21080bd
dubuwater <genzi0524@gmail.com>
2025-05-24 19:28:24 +0900

    5.24_sorting_added

--- DashBoard/views.py
+++ DashBoard/views.py
@@ -1,3 +1,4 @@
+import locale
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from .models import MemberInfo, MemberImageInfo
@@ -11,31 +12,70 @@ def about_view(request):
 def rate_view(request):
     query = request.GET.get('q', '')
     if query:
-        members = MemberInfo.objects.filter(
+        members = list(MemberInfo.objects.filter(
             elected_type='비례대표',
             name__icontains=query
-        ).order_by('party', 'name')   # 소속정당 > 이름 순 정렬
+        ))
     else:
-        members = MemberInfo.objects.filter(
+        members = list(MemberInfo.objects.filter(
             elected_type='비례대표'
-        ).order_by('party', 'name')
-    paginator = Paginator(members, 15)
+        ))
+
+    # locale 설정 (리눅스/맥: 'ko_KR.utf8', 윈도우: 'Korean_Korea.949')
+    try:
+        locale.setlocale(locale.LC_COLLATE, 'ko_KR.utf8')
+    except locale.Error:
+        try:
+            locale.setlocale(locale.LC_COLLATE, 'Korean_Korea.949')
+        except locale.Error:
+            # locale 설정 실패 시 그냥 기본정렬 사용
+            pass
+
+    # 소속정당 > 이름 순으로 한글 정렬
+    members_sorted = sorted(
+        members,
+        key=lambda m: (
+            locale.strxfrm(m.party),
+            locale.strxfrm(m.name)
+        )
+    )
+
+    paginator = Paginator(members_sorted, 15)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return render(request, 'rate.html', {'page_obj': page_obj, 'query': query})
 
 def local_view(request):
     query = request.GET.get('q', '')
+    # 전체 쿼리셋 불러오기 (정렬 없이)
     if query:
-        members = MemberInfo.objects.filter(
+        members = list(MemberInfo.objects.filter(
             elected_type='지역구',
             name__icontains=query
-        ).order_by('party', 'name')   # 소속정당 > 이름 순 정렬
+        ))
     else:
-        members = MemberInfo.objects.filter(
+        members = list(MemberInfo.objects.filter(
             elected_type='지역구'
-        ).order_by('party', 'name')
-    paginator = Paginator(members, 15)
+        ))
+        
+    try:
+        locale.setlocale(locale.LC_COLLATE, 'ko_KR.utf8')
+    except locale.Error:
+        try:
+            locale.setlocale(locale.LC_COLLATE, 'Korean_Korea.949')
+        except locale.Error:
+            # locale 설정 실패 시 그냥 기본정렬 사용
+            pass
+        
+    members_sorted = sorted(
+        members,
+        key=lambda m: (
+            locale.strxfrm(m.party),
+            locale.strxfrm(m.name)
+        )
+    )
+    
+    paginator = Paginator(members_sorted, 15)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return render(request, 'local.html', {'page_obj': page_obj, 'query': query})
